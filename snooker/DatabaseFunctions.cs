9b66e125ea5df4c13665abe0162f1d639a7d1f42
Faizan Ejaz laptop <mirzafaizanejaz@gmail.com>
2018-02-28 19:40:26 +0500

    added function to save player

--- snooker/DatabaseFunctions.cs
+++ snooker/DatabaseFunctions.cs
@@ -9,10 +9,18 @@ namespace snooker
     class DatabaseFunctions
     {
         // Register Player
-
+        public int saveNewPlayer(string name, string no)
+        {
+            string query = @"insert into players values('"+ no +"' , '"+name+"');";
+            ConnectToDB ctd = new ConnectToDB();
+            ctd.ConnectToDataBase();
+            int res = ctd.UpdateQuery(query);
+            ctd.CloseConnection();
+            return res;
+        }
         // Save Match Data
 
         /* write a function to save a single transaction data of a single match data { player name, no , date, time, bill} */
-      
+
     }
 }
