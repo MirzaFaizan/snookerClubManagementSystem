832d53fadced0235a115ef8d2743d2dec5ec922b
Faizan Ejaz laptop <mirzafaizanejaz@gmail.com>
2018-02-28 17:09:15 +0500

    added timer

--- snooker/Form1.Designer.cs
+++ snooker/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace snooker
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,17 +29,21 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.btnTableOne = new System.Windows.Forms.Button();
+            this.TimeTableOne = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -48,129 +52,182 @@
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Controls.Add(this.btnTableOne);
+            this.panel1.Controls.Add(this.TimeTableOne);
+            this.panel1.Location = new System.Drawing.Point(9, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(356, 240);
+            this.panel1.Size = new System.Drawing.Size(267, 195);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // btnTableOne
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.label1.Location = new System.Drawing.Point(97, 92);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(159, 48);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "0:00:00";
+            this.btnTableOne.Location = new System.Drawing.Point(94, 120);
+            this.btnTableOne.Name = "btnTableOne";
+            this.btnTableOne.Size = new System.Drawing.Size(75, 23);
+            this.btnTableOne.TabIndex = 6;
+            this.btnTableOne.Text = "Start";
+            this.btnTableOne.UseVisualStyleBackColor = true;
+            this.btnTableOne.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // TimeTableOne
+            // 
+            this.TimeTableOne.AutoSize = true;
+            this.TimeTableOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.TimeTableOne.Location = new System.Drawing.Point(73, 75);
+            this.TimeTableOne.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.TimeTableOne.Name = "TimeTableOne";
+            this.TimeTableOne.Size = new System.Drawing.Size(130, 39);
+            this.TimeTableOne.TabIndex = 5;
+            this.TimeTableOne.Text = "0:00:00";
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.button7);
             this.panel2.Controls.Add(this.label2);
-            this.panel2.Location = new System.Drawing.Point(384, 12);
+            this.panel2.Location = new System.Drawing.Point(288, 10);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(356, 240);
+            this.panel2.Size = new System.Drawing.Size(267, 195);
             this.panel2.TabIndex = 6;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(96, 117);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.label2.Location = new System.Drawing.Point(97, 92);
+            this.label2.Location = new System.Drawing.Point(73, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(159, 48);
+            this.label2.Size = new System.Drawing.Size(130, 39);
             this.label2.TabIndex = 5;
             this.label2.Text = "0:00:00";
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.button9);
             this.panel3.Controls.Add(this.label3);
-            this.panel3.Location = new System.Drawing.Point(12, 258);
+            this.panel3.Location = new System.Drawing.Point(9, 210);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(356, 240);
+            this.panel3.Size = new System.Drawing.Size(267, 195);
             this.panel3.TabIndex = 6;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(94, 117);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.label3.Location = new System.Drawing.Point(97, 92);
+            this.label3.Location = new System.Drawing.Point(73, 75);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(159, 48);
+            this.label3.Size = new System.Drawing.Size(130, 39);
             this.label3.TabIndex = 5;
             this.label3.Text = "0:00:00";
             // 
             // panel4
             // 
+            this.panel4.Controls.Add(this.button8);
             this.panel4.Controls.Add(this.label4);
-            this.panel4.Location = new System.Drawing.Point(384, 258);
+            this.panel4.Location = new System.Drawing.Point(288, 210);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(356, 240);
+            this.panel4.Size = new System.Drawing.Size(267, 195);
             this.panel4.TabIndex = 6;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(96, 117);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.label4.Location = new System.Drawing.Point(97, 92);
+            this.label4.Location = new System.Drawing.Point(73, 75);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(159, 48);
+            this.label4.Size = new System.Drawing.Size(130, 39);
             this.label4.TabIndex = 5;
             this.label4.Text = "0:00:00";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(758, 12);
+            this.button2.Location = new System.Drawing.Point(568, 10);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(145, 69);
+            this.button2.Size = new System.Drawing.Size(109, 56);
             this.button2.TabIndex = 8;
-            this.button2.Text = "button2";
+            this.button2.Text = "Bill";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(758, 104);
+            this.button3.Location = new System.Drawing.Point(568, 70);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(145, 69);
+            this.button3.Size = new System.Drawing.Size(109, 56);
             this.button3.TabIndex = 9;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(758, 429);
+            this.button4.Location = new System.Drawing.Point(568, 349);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(145, 69);
+            this.button4.Size = new System.Drawing.Size(109, 56);
             this.button4.TabIndex = 10;
             this.button4.Text = "Login";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // button5
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.button5.Location = new System.Drawing.Point(568, 130);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 56);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(915, 516);
+            this.ClientSize = new System.Drawing.Size(686, 419);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainForm";
             this.Text = "Snooker Club";
-            this.UseWaitCursor = true;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -187,17 +244,21 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label TimeTableOne;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnTableOne;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
     }
 }
 
--- snooker/Form1.cs
+++ snooker/Form1.cs
@@ -7,21 +7,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Diagnostics;
 namespace snooker
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        private Timer _timer;
+
+        private DateTime _startTime = DateTime.MinValue;
+        private TimeSpan _currentElapsedTime = TimeSpan.Zero;
+        private TimeSpan _totalElapsedTime = TimeSpan.Zero;
+
+        private bool _timerRunning = false;
+
+
+        private bool tableOne = true,tableTwo= true,tableThree=true,tableFour = true;
+        public MainForm()
         {
             InitializeComponent();
+            _timer = new Timer();
+            _timer.Interval = 1000;
+            _timer.Tick += new EventHandler(_timer_Tick);
+        }
+        void _timer_Tick(object sender, EventArgs e)
+        {
+
+            var timeSinceStartTime = DateTime.Now - _startTime;
+            timeSinceStartTime = new TimeSpan(timeSinceStartTime.Hours,
+                                              timeSinceStartTime.Minutes,
+                                              timeSinceStartTime.Seconds);
+            _currentElapsedTime = timeSinceStartTime + _totalElapsedTime;
+            TimeTableOne.Text = timeSinceStartTime.ToString();
         }
 
-        
+
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
         }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            
+            if (this.tableOne == true) {
+                GameStart();
+                btnTableOne.Text = "Stop";
+                this.tableOne = false;
+            }
+            else
+            {
+                GameStop();
+                btnTableOne.Text = "Start";
+            }
+
+        }
+
+        public void GameStart()
+        {
+            if (!_timerRunning)
+            {
+                // Set the start time to Now
+                _startTime = DateTime.Now;
+
+                // Store the total elapsed time so far
+                _totalElapsedTime = _currentElapsedTime;
+
+                _timer.Start();
+                _timerRunning = true;
+            }
+
+        }
+
+        public void GameStop()
+        {
+            if (_timerRunning)
+            {
+                _timer.Stop();
+                _timerRunning = false;
+            }
+        }
+
     }
 }
